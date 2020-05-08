93b9645825899883ef6ace6771474f926f03436c
Christopher Rodriguez <c9r7z@users.noreply.github.com>
2020-05-08 09:59:01 -0700

    pushing script

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # jwt-parser
 A quick JavaScript to parse a json web token.
+
+# Use
+
+1. Run the script with `node jwtParse.js` with an argument of `—token=“TOKEN”`
+
---
+++ jwtParse.js
@@ -0,0 +1,30 @@
+let args = process.argv.slice(2);
+let keyValues = args.map(a => a.split('='));
+keyValues.forEach(kvp => kvp[0] = kvp[0].replace('--', ''));
+
+const params = {};
+
+keyValues.forEach(kvp => params[kvp[0]] = kvp[1]);
+
+if (params.token) {
+    const token = params.token;
+    const tokenSplit = token.split('.').map(atob);
+    const parsedTokenComponents = tokenSplit.slice(0, 2);
+
+    console.log('Token can be found below');
+    console.log('====================================');
+    console.log('');
+    console.log(parsedTokenComponents);
+
+} else {
+    console.error('arg --token was not present or had no value!');
+    process.exit(1);
+}
+
+function atob(s) {
+    try {
+        return new Buffer(s, 'base64').toString('binary');
+    } catch (e) {
+        return s;
+    }
+}
